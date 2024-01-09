b1588820849d08c17e37c14d6415856ae97f67a1
naanahi <ryuu_yoshi_22_0417@yahoo.co.jp>
2024-01-09 21:26:57 +0900

    CGIの分離

--- cgi-bin/test.py
+++
@@ -1,8 +0,0 @@
-# test.py
- 
-# HTTPの仕様によりコンテンツがtext/htmlだよと示す
-print("Content-Type: text/html")
-# 上記宣言は終わりだよというマークとして改行のみを表示
-print() 
- 
-print('ケンヂまるだよ')
--- cgi-bin/webapi_v1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python3.7
-# -*- coding: UTF-8 -*-
-
-import sys, json
-
-result_json = {'key': 'value'}
-
-print('Content-Type:application/json\n\n')
-print(json.dumps(result_json))
